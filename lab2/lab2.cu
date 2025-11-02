21d2f6a4c03bad5e66f1eead865d1fdbf1824fd1
Lavanda-da <sekti2004@yandex.ru>
2025-11-03 02:56:15 +0300

    Fix problem constant gorizont color

--- lab2/lab2.cu
+++ lab2/lab2.cu
@@ -24,23 +24,25 @@ __global__ void kernel(cudaTextureObject_t tex, uchar4 *out, int w, int h, int n
     int delta_w = w / new_w;
     int delta_h = h / new_h;
     int frame_x, frame_y;
-    int r, g, b;
+    int r, g, b, a;
     uchar4 p;
-    for(y = idy ; y < h; y += offsety) {
-        for(x = idx; x < w; x += offsetx) {
-            r = 0, g = 0, b = 0;
+    for(y = idy * delta_h; y < h; y += offsety) {
+        for(x = idx * delta_w; x < w; x += offsetx) {
+            r = 0, g = 0, b = 0, a = 0;
             for(frame_x = 0; frame_x < delta_w; ++frame_x) {
                 for(frame_y = 0; frame_y < delta_h; ++frame_y) {
-                    p = tex2D<uchar4>(tex, x * delta_w + frame_x, y * delta_h + frame_y);
+                    p = tex2D<uchar4>(tex, x + frame_x, y + frame_y);
                     r += p.x;
                     g += p.y;
                     b += p.z;
+                    a += p.w;
                 }
             }
             r /= (delta_w * delta_h);
             g /= (delta_w * delta_h);
             b /= (delta_w * delta_h);
-            out[y * new_w + x] = make_uchar4(r, g, b, 0);
+            a /= (delta_w * delta_h);
+            out[(y / delta_h) * new_w + x / delta_w] = make_uchar4(r, g, b, a);
         }
     }
 }
